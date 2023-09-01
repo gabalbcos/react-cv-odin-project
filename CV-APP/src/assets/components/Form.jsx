049b6333d9ff650919070fac01a0d45ebab460e0
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-09-01 18:51:31 -0300

    add form button to change edit/save

--- CV-APP/src/assets/components/Form.jsx
+++ CV-APP/src/assets/components/Form.jsx
@@ -1,27 +1,37 @@
 import {useState} from "react";
 
 function General() {
-    const [formData, setFormData] = useState({name:'', email:'', about:''});
+    const [formData, setFormData] = useState({name:'', email:'', about:'', isEditing: true, buttonText: "save"});
   
     const handleChange = (e) => {
         const {name, value} = e.target;
         setFormData((previousFormData) => ({...previousFormData, [name]:value }));
     };
   
+    const handleClick = (e) => {
+        setFormData((previousFormData) => ({...previousFormData, isEditing: !formData.isEditing}))
+        (formData.isEditing ? (setFormData((previousFormData) => ({...previousFormData, buttonText: "edit"})))
+        : (setFormData((previousFormData) => ({...previousFormData, buttonText: "save"}))))
+    };
+
+
     return (
-    <div>
     
+    <div>
+    {formData.isEditing ? (<>
     <label htmlFor="name">Name:</label>
     <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
-    <p>Your Name is: {formData.name}</p>
-
+    
     <label htmlFor="email">E-mail:</label>
     <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
-    <p>Your Name is: {formData.email}</p>
 
     <label htmlFor="about">About:</label>
-    <input type="text" id="aboute" name="about" value={formData.about} onChange={handleChange}/>
-    <p>Your Name is: {formData.about}</p>
+    <input type="text" id="aboute" name="about" value={formData.about} onChange={handleChange}/></>)
+:
+    (<><p>{formData.name}</p>
+    <p>{formData.email}</p>
+    <p>{formData.about}</p></>)}
+    <input type='button' name="isEditing" id="isEditing" value={formData.buttonText} onClick={handleClick}/>
     </div>
     )
 }
