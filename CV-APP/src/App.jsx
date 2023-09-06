218b32276f12c556e5a3ccfc893965f973a28b4f
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-09-05 22:03:44 -0300

    change the CRUD method

--- CV-APP/src/App.jsx
+++ CV-APP/src/App.jsx
@@ -1,26 +1,22 @@
 import { useState } from 'react'
 import { PersonalInfo, EducationInfo } from './assets/components/Forms'
-import './App.css'
 import Create from './assets/components/Create'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
     <h1>Personal Info:</h1>
     <PersonalInfo />
     <h1>Education Info</h1>
     <EducationInfo />
     <h1>Experience</h1>
-    <Router>
-      <div>
-      <Route exact path='/create' component={Create} />
-        </div>
-    </Router>
-    </>
+    <Create/>
+      </div>
+  
   )
 }
 
