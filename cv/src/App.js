dbbe0addc957797e918878791f48c4dafd8d3710
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-09-06 09:18:06 -0300

    add initial form inputs

--- cv/src/App.js
+++ cv/src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>CRUD - Work Experience Form</h1>
+     <h1>CRUD - Job Experience Form</h1>
+     <h2>Job Experience</h2>
+     <form>
+      <label>Company Name:</label>
+      <input type='text' name='companyName' placeholder='Type company name'/><br/><br/>
+      <label>Position Name:</label>
+      <input type='text' name='positionName' placeholder='Type the name of your position'/><br/><br/>
+      <label>Start Date:</label>
+      <input type='date' name='startDate'/><br/><br/>
+      <label>End Date:</label>
+      <input type='date' name='endDate'/><br/><br/>
+     </form>
     </div>
   );
 }
